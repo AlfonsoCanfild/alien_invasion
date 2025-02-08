c730bfaaf58677dd13b3500689a85cceadab7fd2
Alfonso <alfcanfild@gmail.com>
2025-02-08 09:31:28 -0300

    alterações do dia 08-02

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,16 +16,38 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
 
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
             self.clock.tick(60)
 
 
--- settings.py
+++ settings.py
@@ -4,4 +4,5 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (109, 139, 143)
+        self.ship_speed = 4
 
