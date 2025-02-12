c27017bbbb5d3356916966d91c4b7864bc0add2c
Alfonso <alfcanfild@gmail.com>
2025-02-12 09:29:40 -0300

    update 12/02

--- settings.py
+++ settings.py
@@ -1,8 +1,13 @@
 class Settings:
 
     def __init__(self):
+        # config da tela
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (109, 139, 143)
         self.ship_speed = 4
-
+        # config dos tiros
+        self.bullet_speed = 5.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
